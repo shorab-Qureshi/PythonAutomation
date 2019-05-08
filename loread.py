b58947df5c0fbcf9d6b7685ef35c8c66032a1dde
shorab-Qureshi <47708750+shorab-Qureshi@users.noreply.github.com>
2019-05-08 09:36:30 +0530

    Update loread.py

--- loread.py
+++ loread.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3.6
 import os
 os.chdir("/var/log")
-cmd="sudo cat auth.log | grep -i 'Accepted Publickey'"
-std=os.system(cmd)
-
-try: 
+cmd="sudo cat auth.log | grep -i 'Accepted Publickey'> output.txt"
+try:
     day=input("Enter the day to which you want to see the logs from log file:")
 except ValueError as e:
-    print("Plz enter the valid date",e)
+    print("Plz do not enter future date",e)
 def dayCheckLog(day):
-    with open("/home/shorab/basicpython/output.txt") as f:
+    with open("/var/log/output.txt") as f:
         with open("log.txt","w") as f1:
             for line in f:
                 words=line.split()
@@ -17,3 +14,4 @@ def dayCheckLog(day):
                     if word==day:
                         f1.write(line)
 dayCheckLog(day)
+
